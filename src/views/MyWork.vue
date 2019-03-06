1973d4d75cbbb9adf35693bf4c786595a0e80287
Adrian Rocke <business@adrianrocke.com>
2019-03-05 18:10:10 -0600

    move mywork css into component

--- src/views/MyWork.vue
+++ src/views/MyWork.vue
@@ -6,25 +6,24 @@
     <div class="section bg-white text-grey-darkest">
       <h2>
         <img
-          class="align-middle"
           src="@/assets/pivot.png"
           alt="Pivot Interactives Logo"
         >
-        <span class="align-middle ml-4">Pivot Interactives</span>
+        <span>Pivot Interactives</span>
       </h2>
-      <a class="mt-2 block" href="www.pivotinteractives.com">www.pivotinteractives.com</a>
-      <p class="mt-3 leading-normal">
+      <a href="//www.pivotinteractives.com">www.pivotinteractives.com</a>
+      <p>
         Pivot Interactives uses slow motion video and unique measurement tools to enable students to discover scientific models of real events.
         An online library of activities covers the breadth of physics, while biology and chemistry are currently in development.
         Additionally, teachers can create activities using their own videos or with videos from their students.
       </p>
-      <h3 class="mt-6">My Involvement</h3>
-      <p class="mt-3 leading-normal">
+      <h3>My Involvement</h3>
+      <p>
         I've been working as a fullstack developer with Pivot Interactives for four years.
         I've worked on just about every aspect of the product, and continue to prototype and build new features.
       </p>
-      <h3 class="mt-6">Tech Stack</h3>
-      <ul class="mt-3 leading-normal list-reset">
+      <h3>Tech Stack</h3>
+      <ul>
         <li><span class="font-bold text-sm">BACKEND:</span> Express, Mongoose, Mongo</li>
         <li><span class="font-bold text-sm">FRONTEND:</span> Vue, Sass, Backbone, Handlebars, Webpack</li>
         <li><span class="font-bold text-sm">TESTING:</span> Mocha, Chai, Karma</li>

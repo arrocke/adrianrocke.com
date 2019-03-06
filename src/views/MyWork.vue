aaeaeaa9dfac571350ea7e0d0659bc6ee77e07c3
Adrian Rocke <business@adrianrocke.com>
2019-03-05 19:57:09 -0600

    added this site to my work

--- src/views/MyWork.vue
+++ src/views/MyWork.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="flex-grow flex flex-col items-stretch">
-    <div class="section">
+    <div class="section py-6">
       <h1 class="text-4xl">My Work</h1>
     </div>
-    <div class="section bg-white text-grey-darkest">
+    <div class="section py-10 bg-white text-grey-darkest">
       <h2>
         <img
           src="@/assets/pivot.png"
@@ -22,11 +22,27 @@
         I've been working as a fullstack developer with Pivot Interactives for four years.
         I've worked on just about every aspect of the product, and continue to prototype and build new features.
       </p>
-      <h3>Tech Stack</h3>
+      <h3>Tech</h3>
       <ul>
         <li><span class="font-bold text-sm">BACKEND:</span> Express, Mongoose, Mongo</li>
-        <li><span class="font-bold text-sm">FRONTEND:</span> Vue, Sass, Backbone, Handlebars, Webpack</li>
-        <li><span class="font-bold text-sm">TESTING:</span> Mocha, Chai, Karma</li>
+        <li><span class="font-bold text-sm">FRONTEND:</span> Vue, Sass, Backbone, Handlebars</li>
+        <li><span class="font-bold text-sm">DEVELOPMENT:</span> Webpack, Mocha, Chai, Karma, Heroku</li>
+      </ul>
+    </div>
+    <div class="section py-10 bg-white text-grey-darkest mt-10">
+      <h2>
+        <img class="rounded-full" src="@/assets/headshot.jpg" alt="Adrian Rocke's Headshot">
+        <span>This Site</span>
+      </h2>
+      <a href="//adrianrocke.com">adrianrocke.com</a>
+      <p>
+        While I have no formal education in design, I have picked up a few tricks along the way.
+        I've used what I've learned to design and build this site.
+      </p>
+      <h3>Tech</h3>
+      <ul>
+        <li><span class="font-bold text-sm">FRONTEND:</span> Vue, Sass, Tailwind</li>
+        <li><span class="font-bold text-sm">DEVELOPMENT:</span> Vue-CLI</li>
       </ul>
     </div>
   </main>

408e1cc767444d3e8eab59e4e6cd29446103d98f
Adrian Rocke <business@adrianrocke.com>
2019-05-27 23:40:56 -0500

    constrain line length

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,8 +48,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#22292f`,
+        theme_color: `#22292f`,
         display: `minimal-ui`,
         icon: `src/assets/icons/gatsby.png`, // This path is relative to the root of the site.
       },

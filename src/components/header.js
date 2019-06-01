47380fc1d4e2f0d0a1b7fa93a508a8dc92b4559b
Adrian Rocke <business@adrianrocke.com>
2019-05-31 20:04:28 -0500

    navigation accessibility attempt

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,8 @@ const Header = ({siteTitle, navLinks}) => {
   const [menuVisible, setMenuVisibility] = useState(false)
   const [transitioning, setTransitioning] = useState(false)
 
-  const toggleMenu = () => {
+  const toggleMenu = e => {
+    e.stopPropagation()
     setTransitioning(true)
     setMenuVisibility(visible => !visible)
   }
@@ -13,6 +14,12 @@ const Header = ({siteTitle, navLinks}) => {
   const hideMenu = () => {
     setTransitioning(true)
     setMenuVisibility(false)
+  }
+
+  const onKeyDown = e => {
+    if (e.key === 'Escape') {
+      hideMenu()
+    }
   }
 
   const links = navLinks.map(
@@ -25,19 +32,28 @@ const Header = ({siteTitle, navLinks}) => {
       {siteTitle.toUpperCase()}
     </span>
     <div className="flex-grow h-12" />
-    <button
-      type="button"
-      className="mb-2 w-10 h-10 z-10 bg-menu bg-no-repeat bg-bottom lg:hidden"
-      onClick={toggleMenu}
-      aria-label="toggle menu"
-    />
     <nav
-      className={`fixed inset-0 z-20 lg:inset-auto lg:static lg:mt-0 lg:opacity-100 lg:visible ${menuVisible ? '' : 'invisible opacity-0'} ${transitioning ? 'transition-opacity' : ''}`}
       onClick={hideMenu}
       onTransitionEnd={() => setTransitioning(false)}
+      className="z-20"
+      onKeyDown={onKeyDown}
     >
+      <button
+        type="button"
+        className="relative mb-2 w-10 h-10 bg-menu bg-no-repeat bg-bottom z-10 lg:hidden"
+        onClick={toggleMenu}
+        aria-label="toggle menu"
+        aria-expanded={menuVisible}
+        aria-haspopup="true"
+      />
       <div
-        className="h-full flex flex-col items-center bg-black justify-center overflow-y-hidden lg:flex-row lg:pin-none lg:mb-0 lg:pb-0"
+        className={`
+          fixed flex flex-col items-center bg-black justify-center inset-0
+          lg:static lg:inset-auto lg:my-0 lg:pb-0 lg:opacity-100 lg:visible lg:flex-row 
+          ${menuVisible ? '' : 'invisible opacity-0'}
+          ${transitioning ? 'transition-opacity' : ''}
+        `}
+        hidden={!menuVisible}
       >
         {links}
       </div>

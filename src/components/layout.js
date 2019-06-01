7c524a065ab465523856079d268b8f6ecfb69d0b
Adrian Rocke <business@adrianrocke.com>
2019-05-31 20:23:46 -0500

    now deploy

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,8 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 
-import Header from '~components/Header'
-import Footer from '~components/Footer'
+import Header from '~components/header'
+import Footer from '~components/footer'
 
 import "~styles/layout.css"
 
